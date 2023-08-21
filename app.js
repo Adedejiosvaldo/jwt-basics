f3e2b0f591176eb4d296818ad6c27965caa1a376
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-21 11:01:59 +0100

    Basic file part

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.use(express.static("./public"));
+app.use(express.json);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

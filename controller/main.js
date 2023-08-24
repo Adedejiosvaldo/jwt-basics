1fa4344fa92b15b9cb0757f2b69077b4f954de1c
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-24 12:04:46 +0100

    Feature - Added jwt token

--- controller/main.js
+++ controller/main.js
@@ -1,5 +1,5 @@
 const CustomAPIError = require("../errors/custom-error");
-
+const jwt = require("jsonwebtoken");
 // validation Types
 // 1. Mongoose
 // 2. JOi
@@ -10,9 +10,15 @@ const login = async (req, res) => {
     // res.send("You have to input a username and username");
     throw new CustomAPIError("Please Provide Email and Password", 400);
   }
-
   console.log(username, password);
-  res.send("Fake Login Route /Signup");
+
+  const id = new Date().getMilliseconds;
+  console.log(username, password);
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  console.log(username, password);
+  res.status(200).json({ msg: "Fake Login Route /Signup", token });
 };
 const dashBoard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);

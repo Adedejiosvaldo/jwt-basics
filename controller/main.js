ba61a5eeed847a430d839db919cc72e2e9a361c7
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-09 12:47:01 +0100

    Fix - Improved auth middle ware

--- controller/main.js
+++ controller/main.js
@@ -16,26 +16,17 @@ const login = async (req, res) => {
   const token = jwt.sign({ id, username }, process.env.JWT_SECRETS, {
     expiresIn: "30d",
   });
-  console.log(token);
 
   res.status(200).json({ msg: "User Created!", token });
 };
 
 const dashBoard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomAPIError("No Token Provided", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-
+  console.log("dashbpard", req.user);
   try {
-    const decode = jwt.verify(token, process.env.JWT_SECRETS);
-    console.log(decode);
+    const secureDate = Math.floor(Math.random() * 100);
     res.status(200).json({
-      msg: `Hello ${decode.username}`,
-      secret: `Here is your secret token ${token}`,
+      msg: `Hello ${req.user.username}`,
+      secret: `Here is your personalized data  ${secureDate}`,
     });
   } catch (error) {
     throw new CustomAPIError("Not authorized to access route", 401);
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,22 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+
 const authMiddleWare = async (req, res, next) => {
-  console.log(req.headers.authorization);
-  next();
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No Token Provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRETS);
+    const { id, username } = decode;
+    req.user = { id, username };
+    next();
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access route - auth", 401);
+  }
 };
 module.exports = authMiddleWare;

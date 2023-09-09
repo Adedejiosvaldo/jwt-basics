8dcabe5810e1291296812efe1c8cc2830aea50c1
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-09-09 12:23:20 +0100

    Feature - AuthMiddleware

---
+++ middleware/auth.js
@@ -0,0 +1,5 @@
+const authMiddleWare = async (req, res, next) => {
+  console.log(req.headers.authorization);
+  next();
+};
+module.exports = authMiddleWare;
--- routes/jwt.js
+++ routes/jwt.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { login, dashBoard } = require("../controller/main");
+const authMiddleWare = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/login").get().post(login);
-router.route("/dashboard").get(dashBoard);
+router.route("/login").post(login);
+router.route("/dashboard").get(authMiddleWare, dashBoard);
 module.exports = router;
